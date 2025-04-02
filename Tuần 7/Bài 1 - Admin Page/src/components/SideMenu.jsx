3fe68ccbb35bafa1664141af5e97bb4843e2477c
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-04-02 11:30:14 +0700

    Layout CSS, API Overview

---
+++ Tuần 7/Bài 1 - Admin Page/src/components/SideMenu.jsx
@@ -0,0 +1,47 @@
+import {
+  Blocks,
+  ChartSpline,
+  FolderOpenDot,
+  LayoutDashboard,
+  MessageSquareText,
+  UsersRound,
+} from "lucide-react";
+import logo from "../assets/Logo_IUH.png";
+
+import React from "react";
+
+export const SideMenu = () => {
+  return (
+    <div className="border-gray-300 border-2 flex flex-col items-center py-4">
+      <img src={logo} alt="" className="w-[150px]" />
+      <nav className="my-12 w-full">
+        <ul>
+          <li className="py-2 flex flex-row justify-left space-x-2 hover:bg-pink-300/50 w-full px-2 hover:cursor-pointer">
+            <LayoutDashboard />
+            <p className="text-gray-500 text-lg font-bold">Dashboard</p>
+          </li>
+          <li className="py-2 flex flex-row justify-left space-x-2 hover:bg-pink-300/50 w-full px-2 hover:cursor-pointer">
+            <FolderOpenDot />
+            <p className="text-gray-500 text-lg font-bold">Projects</p>
+          </li>
+          <li className="py-2 flex flex-row justify-left space-x-2 hover:bg-pink-300/50 w-full px-2 hover:cursor-pointer">
+            <UsersRound />
+            <p className="text-gray-500 text-lg font-bold">Teams</p>
+          </li>
+          <li className="py-2 flex flex-row justify-left space-x-2 hover:bg-pink-300/50 w-full px-2 hover:cursor-pointer">
+            <ChartSpline />
+            <p className="text-gray-500 text-lg font-bold">Analytics</p>
+          </li>
+          <li className="py-2 flex flex-row justify-left space-x-2 hover:bg-pink-300/50 w-full px-2 hover:cursor-pointer">
+            <MessageSquareText />
+            <p className="text-gray-500 text-lg font-bold">Message</p>
+          </li>
+          <li className="py-2 flex flex-row justify-left space-x-2 hover:bg-pink-300/50 w-full px-2 hover:cursor-pointer">
+            <Blocks />
+            <p className="text-gray-500 text-lg font-bold">Intergrations</p>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
